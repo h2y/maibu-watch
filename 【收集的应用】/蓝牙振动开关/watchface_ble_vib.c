c72e61017f84fb26edff19bd18d632b107969073
Moshel <hzy961016@163.com>
2017-08-01 11:11:25 +0800

    蓝牙震动开关 深度重构
    - 重构90%的代码，耗电量明显减少且占用空间更少
    - 蓝牙状态扫描周期 1s -> 5s
    - 关闭蓝牙功能后，彻底不占用运行资源
    - 仅在断开蓝牙时震动
    - 界面重排，增加图标显示，更加美观
    
    * 原空间占用：
    this app used size :            1272
    
    text section       :    1084
    rel text section   :    264
    data section       :    4
    bss section        :    0
    rodata section     :    132
    symtab section     :    560
    strtab section     :    415
    
    * 现空间占用：
    this app used size :            857
    
    text section       :    744
    rel text section   :    248
    data section       :    1
    bss section        :    8
    rodata section     :    52
    symtab section     :    688
    strtab section     :    524

--- 【收集的应用】/蓝牙振动开关/watchface_ble_vib.c
+++ 【收集的应用】/蓝牙振动开关/watchface_ble_vib.c
@@ -1,203 +1,125 @@
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+//TODO：my own uuid
+//TODO: push window to notify
 
 #include "maibu_res.h"
 #include "maibu_sdk.h"
 
 
+//蓝牙状态扫描周期（ms）
+#define BT_SCAN_TIME_HZY 5000
+
+//断开时震动次数
+#define VIBES_TIMES_DISCON 1
+
+//窗口ID
+static int32_t g_triangle_window = 0;
+
+//是否打开蓝牙提醒功能
+static bool g_switch_BLE = 1;
 
 
-static uint8_t g_triangle_layer_h = 0, g_triangle_layer_m = 0;
-static uint32_t g_triangle_window = 0;
-
-static uint32_t g_switch_BLE = 0;
-
-/*窗口图层位置*/
-/*背景图层*/
-#define TRIANGLE_BG_ORIGIN_X			0
-#define TRIANGLE_BG_ORIGIN_Y			0
-#define TRIANGLE_BG_SIZE_H			128
-#define TRIANGLE_BG_SIZE_W			128
-
-/*蓝牙文本图层位置*/
-#define SHOW_MORE_D_ORIGIN_X		9	
-#define SHOW_MORE_D_ORIGIN_Y		54
-#define SHOW_MORE_D_SIZE_H			16	
-#define SHOW_MORE_D_SIZE_W			114
-
-	
-
-//蓝牙图层id
-char g_show_more_str_week[21] = {"0"};
-/*定时器状态*/
-static uint8_t g_request_ble_timer		= 0;
-//蓝牙信息
-static int32_t g_official_bledis = 0;
-
-void get_ble_status(date_time_t tick_time, uint32_t millis, void *context);
-
-P_Window init_triangle_watch(void);
+/*定时器ID */
+static uint8_t g_request_ble_timer = 0;
+//上一次的蓝牙信息 1连接 2断开 0未知
+static uint8_t bt_last_state = 0;
 
 
+//函数声明
+static void get_ble_status(date_time_t tick_time, uint32_t millis, void *context);
+static P_Window init_triangle_watch(void);
 
 
-
-
-/*
- *--------------------------------------------------------------------------------------
- *     function:  get_bmp_layer
- *    parameter: 
- *       return:
- *  description:  
- * 	  other:
- *--------------------------------------------------------------------------------------
- */
-P_Layer get_bmp_layer(GRect *frame_p ,int32_t bmp_key)
+/*定义enter按键事件*/
+static void scroll_select_enter(void *context)
 {
-
-	GBitmap bitmap;
+	maibu_service_vibes_pulse(VibesPulseTypeShort, 1); 
+	g_switch_BLE = !g_switch_BLE;
 	
-	/*获取系统图片*/
-	res_get_user_bitmap(bmp_key, &bitmap);
-
-	//printf("XXXXXXXXXXXXXXXXXXXXXX:%d, %d, %d, %d\n", bitmap.app_id, bitmap.key, bitmap.height, bitmap.width);
-	LayerBitmap lb1 = { bitmap,*frame_p, GAlignLeft};	
-
-	/*图层1*/
-	P_Layer	 layer1 = NULL;
-	layer1 = app_layer_create_bitmap(&lb1);
-	//app_layer_set_bg_color(layer1, GColorBlack);
-
-	return (layer1);
-}
-
-
-//重新载入并刷新窗口所有图层
-void window_reloading(void)
-{
-	/*根据窗口ID获取窗口句柄*/
+	//重新载入窗口
 	P_Window p_old_window = app_window_stack_get_window_by_id(g_triangle_window); 
 	if (NULL != p_old_window)
 	{
 		P_Window p_window = init_triangle_watch();
-		if (NULL != p_window)
-		{
-			g_triangle_window = app_window_stack_replace_window(p_old_window, p_window);
-		}	
+		g_triangle_window = app_window_stack_replace_window(p_old_window, p_window);
 	}
-	
 }
 
-/*定义enter按键事件*/
-void scroll_select_enter(void *context)
-{
-	if (g_switch_BLE == 0)
-	{
-		g_switch_BLE = 1;
-	}else{
-		g_switch_BLE = 0;
-	}
-	window_reloading();
-}
 
 /*定义后退按键事件*/
 static void scroll_select_back(void *context)
 {
 	P_Window p_window = (P_Window)context;
 	if (NULL != p_window)
-	{
 		app_window_stack_pop(p_window);
-	}
 }
 
-//获取蓝牙状态，是否断开。
- void get_ble_status(date_time_t tick_time, uint32_t millis, void *context)
-{
-        uint8_t ble_sta = maibu_get_ble_status();  //获取蓝牙状态
-        if (ble_sta != 1 && ble_sta != 2 ) {
-                if (g_official_bledis == 0 && g_switch_BLE == 1)
-                {
-					g_official_bledis = 1;
-					maibu_service_vibes_pulse(VibesPulseTypeMiddle,1); 
-                }
-        }
-        else if (ble_sta = 1) {
-                if (g_official_bledis == 1 && g_switch_BLE == 1)
-                {
-					g_official_bledis = 0;
-					maibu_service_vibes_pulse(VibesPulseTypeMiddle,1); 
-                }
-        }
-		
-}
 
-/*创建并显示文本图层*/
-int32_t display_target_layerText(P_Window p_window,GRect  *temp_p_frame,enum GAlign how_to_align,enum GColor color,char * str,uint8_t font_type)
+//定时回调：获取蓝牙状态
+static void get_ble_status(date_time_t tick_time, uint32_t millis, void *context)
 {
-	LayerText temp_LayerText = {0};
-	temp_LayerText.text = str;
-	temp_LayerText.frame = *temp_p_frame;
-	temp_LayerText.alignment = how_to_align;
-	temp_LayerText.font_type = font_type;
+	//获取蓝牙状态
+	uint8_t bt_state = maibu_get_ble_status();  
+	if(bt_state==BLEStatusConnected || bt_state==BLEStatusUsing)
+		bt_state = 1;
+	else
+		bt_state = 2;
 	
-	P_Layer p_layer = app_layer_create_text(&temp_LayerText);
 	
-	if(p_layer != NULL)
+	if(bt_state != bt_last_state)
 	{
-		app_layer_set_bg_color(p_layer, color);
-		return app_window_add_layer(p_window, p_layer);
+		bt_last_state = bt_state;
+		
+		//仅断开震动
+		if(bt_state == 2)
+			maibu_service_vibes_pulse(VibesPulseTypeMiddle, VIBES_TIMES_DISCON); 
 	}
-	return 0;
 }
 
 
-P_Window init_triangle_watch(void)
+//生成窗口
+static P_Window init_triangle_watch(void)
 {
 	P_Window p_window = NULL;
 	p_window = app_window_create();
 	if (NULL == p_window)
-	{
 		return NULL;
-	}
 
-	P_Layer bgl = NULL, hl = NULL, ml = NULL, cl1 = NULL, cl2 = NULL;
 
-	struct date_time datetime;
-	app_service_get_datetime(&datetime);
-
-	GRect frame = {{TRIANGLE_BG_ORIGIN_X, TRIANGLE_BG_ORIGIN_Y}, {TRIANGLE_BG_SIZE_H, TRIANGLE_BG_SIZE_W}};
-	bgl = get_bmp_layer(&frame,RES_BITMAP_WATCHFACE_OFFICIAL_BG01);
+	GRect grect = {{8,74},{16,112}};
+	Layer *p_layer;
 	
-
-	/*添加图层到窗口*/
-	app_window_add_layer(p_window, bgl);
+	//创建文本图层
+	char str[22] = "蓝牙振动已开启";
+	if(!g_switch_BLE)
+		sprintf(str, "%s", "蓝牙振动已关闭");
 	
-
-	//添加蓝牙文本图层
-	frame.origin.x = SHOW_MORE_D_ORIGIN_X;
-	frame.origin.y = SHOW_MORE_D_ORIGIN_Y;
-	frame.size.h = SHOW_MORE_D_SIZE_H;
-	frame.size.w = SHOW_MORE_D_SIZE_W;
-
-	//获取蓝牙文本
-	if (g_switch_BLE == 0)
-	{
-		sprintf(g_show_more_str_week, "%s", "蓝牙振动已关闭");	
-	}else{
-		sprintf(g_show_more_str_week, "%s", "蓝牙振动已开启");	
-		maibu_service_vibes_pulse(VibesPulseTypeShort,1); 
-	}
+	LayerText layertext = {str, grect, GAlignCenter, U_ASCII_ARIALBD_24, 0};
+	p_layer = app_layer_create_text(&layertext);
+	app_window_add_layer(p_window, p_layer);
 	
-	display_target_layerText(p_window,&frame,GAlignCenter,GColorBlack,g_show_more_str_week,U_ASCII_ARIALBD_24);
+	//创建图标图层
+	grect.origin.x = 52;		grect.origin.y = 40;
+	grect.size.h   = 24;	    grect.size.w   = 24;
+	GBitmap gbitmap;
+	res_get_user_bitmap(IMAGE_ICON, &gbitmap);
+	LayerBitmap layerbitmap = {gbitmap, grect, GAlignCenter};
+	p_layer = app_layer_create_bitmap(&layerbitmap);
+	app_window_add_layer(p_window, p_layer);
 	
-	app_window_update(p_window);
-
+	
 	/*添加按键事件*/
 	app_window_click_subscribe(p_window, ButtonIdSelect, scroll_select_enter);
 	app_window_click_subscribe(p_window, ButtonIdBack, scroll_select_back);
 
+	//定时器处理
+	if(g_switch_BLE && !g_request_ble_timer)
+		g_request_ble_timer = app_service_timer_subscribe(BT_SCAN_TIME_HZY, get_ble_status, NULL);
+	else if(!g_switch_BLE && g_request_ble_timer)
+	{
+		app_service_timer_unsubscribe(g_request_ble_timer);
+		g_request_ble_timer = 0;
+	}
+	
 	
 	return p_window;
 }
@@ -205,22 +127,14 @@ P_Window init_triangle_watch(void)
 
 int main()
 {
-              /*模拟器模拟显示时打开，打包时屏蔽*/
 	//simulator_init();
 
-	if(g_request_ble_timer == 0)
-	{
-		g_request_ble_timer = app_service_timer_subscribe(1000, get_ble_status, NULL);	
-	}
 
 	P_Window p_window = init_triangle_watch();
 	
-
 	g_triangle_window = app_window_stack_push(p_window);
 
 
-	/*模拟器模拟显示时打开，打包时屏蔽*/
 	//simulator_wait();
-
 	return 0;
 }
