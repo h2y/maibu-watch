427d9dbbcc8a52eeeffe71cebf1ba08fff20e2d9
Moshel <hzy961016@163.com>
2017-08-03 18:52:55 +0800

    睡眠检测 修复日期错位的大bug 继续优化代码，占用量下降到3356

--- 手表应用/睡眠监测/sleep_detect.c
+++ 手表应用/睡眠监测/sleep_detect.c
@@ -23,12 +23,6 @@ typedef struct SleepInfo
 	struct date_time wakeTime;	//起床时间
 }SleepInfo;
 
-//七天各自的总睡眠时长
-static uint16_t weekday_sum[7] = { 0 };
-
-//七天睡眠信息
-static SleepInfo week_info[7] = { 0 };
-
 
 //窗口ID
 static int32_t g_window_id = -1;
@@ -39,18 +33,6 @@ static uint8_t BGM_flag = 0;
 
 static void update_display(void);
 
-
-
-static uint32_t get_sleep_level(uint8_t wday)
-{
-	switch (week_info[wday].sleepQuality)
-	{
-	case 0: return RES_BITMAP_LV_3;
-	case 1: return RES_BITMAP_LV_2;
-	case 2: return RES_BITMAP_LV_1;
-	default: return RES_BITMAP_LV_0;
-	}
-}
 
 
 void button_select_back(void *context)
@@ -86,8 +68,6 @@ static void create_layer_text(P_Window p_window, char* str, GRect* frame, int8_t
 
 static void create_progress_bar(P_Window p_window, uint8_t date_flag, uint8_t percent, ProgressType type)
 {
-	Geometry *p_geometry_array[1];
-
 	uint8_t origin_x = 14, width = 6; //TYPE_OLD_EX
 	enum GColor color = GColorBlack;
 
@@ -112,7 +92,7 @@ static void create_progress_bar(P_Window p_window, uint8_t date_flag, uint8_t pe
 								  {origin_x + width + date_flag * 16, 95},
 								  {origin_x + date_flag * 16, 95} };
 
-	if ((percent > 0) && (100 >= percent))
+	if (percent)
 	{
 		prog_bar_points[0].y += 60 * (100 - percent) / 100;
 		prog_bar_points[1].y += 60 * (100 - percent) / 100;
@@ -122,7 +102,6 @@ static void create_progress_bar(P_Window p_window, uint8_t date_flag, uint8_t pe
 			prog_bar_points[0].y += 2;
 			prog_bar_points[1].y += 2;
 		}
-
 	}
 	else
 	{
@@ -131,12 +110,12 @@ static void create_progress_bar(P_Window p_window, uint8_t date_flag, uint8_t pe
 	}
 
 	Polygon prog_bar = { 4, prog_bar_points };
-	Geometry geometry = { GeometryTypePolygon, FillArea, color,(void*)&prog_bar };
+	Geometry geometry = { GeometryTypePolygon, FillArea, color, (void*)&prog_bar };
 
-	p_geometry_array[0] = &geometry;
-	LayerGeometry prog_bar_struct = { 1, p_geometry_array };
+	Geometry *p_geometry_array = &geometry;
+	LayerGeometry prog_bar_struct = { 1, &p_geometry_array };
 
-	P_Layer	 layer = app_layer_create_geometry(&prog_bar_struct);
+	P_Layer layer = app_layer_create_geometry(&prog_bar_struct);
 	app_window_add_layer(p_window, layer);
 }
 
@@ -160,10 +139,35 @@ static void update_display(void)
 	if (NULL == p_new_window) return;
 
 
-	//公共变量创建
 	struct date_time datetime;
 	app_service_get_datetime(&datetime);
 
+
+	/*
+		获取七日睡眠数据
+	*/
+
+	//七天各自的总睡眠时长
+	static uint16_t weekday_sum[7] = { 0 };
+	//七天睡眠信息
+	static SleepInfo week_info[7] = { 0 };
+
+	uint8_t i = 0, wday;
+	for (; i < 7; i++)
+	{
+		wday = (datetime.wday - 1 + 7 - i) % 7;
+		maibu_get_sleep_info(i, &week_info[wday]);
+
+		if (week_info[wday].deepSleep != 0xffff)
+		{
+			weekday_sum[wday] = week_info[wday].deepSleep + week_info[wday].shallowSleep;
+		}
+		else
+			weekday_sum[wday] = 0;
+	}
+
+
+	//公共变量创建
 	GRect frame_bmp = { { 45, 9 },{ 47, 52 } };
 	uint32_t* p_frame_bmp = (uint32_t*)&frame_bmp;
 
@@ -188,52 +192,63 @@ static void update_display(void)
 		//显示睡眠时长
 		uint16_t array_num[] = { RES_BITMAP_NO_0,RES_BITMAP_NO_1,RES_BITMAP_NO_2,RES_BITMAP_NO_3,RES_BITMAP_NO_4,RES_BITMAP_NO_5,RES_BITMAP_NO_6,RES_BITMAP_NO_7,RES_BITMAP_NO_8,RES_BITMAP_NO_9 };
 
+        uint8_t yesterday_index = datetime.wday - 1 - 1;
 
+        
 		// frame_bmp: 28,31,26,12
 		*p_frame_bmp = 0x0c1a1f1c;
-		res_get_user_bitmap(array_num[weekday_sum[datetime.wday] / 60 / 10], &bmp_temp);
+		res_get_user_bitmap(array_num[weekday_sum[yesterday_index] / 60 / 10], &bmp_temp);
 		create_layer_bmp(p_new_window, &bmp_temp, &frame_bmp);
 
 		frame_bmp.origin.x += 12;
-		res_get_user_bitmap(array_num[(weekday_sum[datetime.wday] / 60) % 10], &bmp_temp);
+		res_get_user_bitmap(array_num[(weekday_sum[yesterday_index] / 60) % 10], &bmp_temp);
 		create_layer_bmp(p_new_window, &bmp_temp, &frame_bmp);
 
 		frame_bmp.origin.x += 24;
-		res_get_user_bitmap(array_num[weekday_sum[datetime.wday] % 60 / 10], &bmp_temp);
+		res_get_user_bitmap(array_num[weekday_sum[yesterday_index] % 60 / 10], &bmp_temp);
 		create_layer_bmp(p_new_window, &bmp_temp, &frame_bmp);
 
 		frame_bmp.origin.x += 12;
-		res_get_user_bitmap(array_num[weekday_sum[datetime.wday] % 60 % 10], &bmp_temp);
+		res_get_user_bitmap(array_num[weekday_sum[yesterday_index] % 60 % 10], &bmp_temp);
 		create_layer_bmp(p_new_window, &bmp_temp, &frame_bmp);
 
 
 		//如果有睡眠数据
-		if (weekday_sum[datetime.wday])
+		if (weekday_sum[yesterday_index])
 		{
 			//显示睡眠质量
 			change_GRect(&frame_bmp, 57, 62, 26, 14);
-			res_get_user_bitmap(get_sleep_level((datetime.wday)), &bmp_temp);
+
+			uint32_t res_bitmap_id = RES_BITMAP_LV_0; //case 3
+			switch (week_info[yesterday_index].sleepQuality)
+			{
+				case 1: res_bitmap_id = RES_BITMAP_LV_2; break;
+				case 0: res_bitmap_id = RES_BITMAP_LV_3; break;
+				case 2: res_bitmap_id = RES_BITMAP_LV_1; 
+			}
+
+			res_get_user_bitmap(res_bitmap_id, &bmp_temp);
 			create_layer_bmp(p_new_window, &bmp_temp, &frame_bmp);
 
 			//显示睡眠时间
 			change_GRect(&frame_bmp, 14, 93, 14, 100);
 			sprintf(str_buf, "%d:%02d - %d:%02d", \
-				week_info[datetime.wday].sleepTime.hour, \
-				week_info[datetime.wday].sleepTime.min, \
-				week_info[datetime.wday].wakeTime.hour, \
-				week_info[datetime.wday].wakeTime.min);
+				week_info[yesterday_index].sleepTime.hour, \
+				week_info[yesterday_index].sleepTime.min, \
+				week_info[yesterday_index].wakeTime.hour, \
+				week_info[yesterday_index].wakeTime.min);
 			create_layer_text(p_new_window, str_buf, &frame_bmp, U_ASCII_ARIALBD_14);
 
 			//显示睡眠数据
 			change_GRect(&frame_bmp, 0, 110, 14, 128);
 			sprintf(str_buf, "深睡%d%% 清醒%d次", \
-				(100 * week_info[datetime.wday].deepSleep + 50) / (week_info[datetime.wday].deepSleep + week_info[datetime.wday].shallowSleep), \
-				week_info[((datetime.wday))].wakeCount);
+				(100 * week_info[yesterday_index].deepSleep + 50) / weekday_sum[yesterday_index], \
+				week_info[yesterday_index].wakeCount);
 			create_layer_text(p_new_window, str_buf, &frame_bmp, U_ASCII_ARIAL_14);
 		}
 
 	}
-	else if (BGM_flag == 1)
+	else //if (BGM_flag == 1)
 	{
 		uint8_t i = 0;
 		uint16_t max_sleep_time = 1;
@@ -265,7 +280,7 @@ static void update_display(void)
 		{
 			uint8_t percent = weekday_sum[i] * 100 / max_sleep_time;
 
-			if (i < datetime.wday)
+			if (i < datetime.wday - 1)
 				create_progress_bar(p_new_window, i, percent, TYPE_CUR);
 			else
 			{
@@ -277,7 +292,7 @@ static void update_display(void)
 		//GRect frame_bmp = {{0,114},{14,128}};
 		*p_frame_bmp = 0x800e7100;
 		if (week_info_num > 0)
-			sprintf(str_buf, "%d日平均时长%d时%d分", week_info_num, week_time_avg / 60, week_time_avg % 60);
+			sprintf(str_buf, "%d日平均睡眠%d时%d分", week_info_num, week_time_avg / 60, week_time_avg % 60);
 		else
 			sprintf(str_buf, "暂无统计");
 
@@ -287,39 +302,16 @@ static void update_display(void)
 
 	/*添加窗口按键事件*/
 	app_window_click_subscribe(p_new_window, ButtonIdDown, button_select_up_down);
-	app_window_click_subscribe(p_new_window, ButtonIdUp, button_select_up_down);
+	app_window_click_subscribe(p_new_window, ButtonIdUp,   button_select_up_down);
 	app_window_click_subscribe(p_new_window, ButtonIdBack, button_select_back);
 
 	g_window_id = app_window_stack_replace_window(p_window, p_new_window);
 }
 
 
-static void getWeekSleepInfo(void)
-{
-	struct date_time datetime;
-	app_service_get_datetime(&datetime);
-
-	uint8_t i = 0, wday;
-	for (; i < 7; i++)
-	{
-		wday = (datetime.wday + 7 - i) % 7;
-		maibu_get_sleep_info(i, &week_info[wday]);
-
-		if (week_info[wday].deepSleep != 0xffff)
-		{
-			weekday_sum[wday] = week_info[wday].deepSleep + week_info[wday].shallowSleep;
-		}
-		else
-			weekday_sum[wday] = 0;
-	}
-}
-
-
 int main(void)
 {
 	g_window_id = app_window_stack_push(app_window_create());
-
-	getWeekSleepInfo();
 
 	update_display();
 
